e90cae511cf977807afddca93d01a77c9d4496b4
StanislavSmolyanko <st.smolyanko@gmail.com>
2021-09-26 11:52:38 +0300

    added conditions to function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно
 
 function getRandomInt(min, max) {
+  if (min < 0 || max < 0) {
+    throw new Error('Значения диапазона чисел могут быть только положительными');
+  }
+
+  if (min > max) {
+    const swap = min;
+    min = max;
+    max = swap;
+  }
+
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
