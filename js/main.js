b24464ed1c71f33b398eb0c017ebf8bfcb77a7d3
StanislavSmolyanko <st.smolyanko@gmail.com>
2021-10-03 21:16:53 +0300

    added getPhotosArr function

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
-/* eslint-disable arrow-body-style */
 // Функция, возвращающая случайное целое число из переданного диапазона включительно
 
-// function getRandomInt(min, max) {
-//   if (min < 0 || max < 0) {
-//     throw new Error('Значения диапазона чисел могут быть только положительными');
-//   }
+function getRandomInt(min, max) {
+  if (min < 0 || max < 0) {
+    throw new Error('Значения диапазона чисел могут быть только положительными');
+  }
 
-//   if (min > max) {
-//     const swap = min;
-//     min = max;
-//     max = swap;
-//   }
+  if (min > max) {
+    const swap = min;
+    min = max;
+    max = swap;
+  }
 
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-// getRandomInt(0,10);
+getRandomInt(0,10);
 
 
 // Функция для проверки максимальной длины строки
@@ -29,27 +28,31 @@ isStringFit('Привет', 7);
 
 // Больше деталей
 
-const getRandomInt = (min, max) => {
-  return _.random(min, max);
+const getPhotosArr = () => {
+  const photoArr = [];
+  const commentsArr = ['Всё отлично!','В целом всё неплохо. Но не всё.','Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.','Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.','Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+
+  const namesArr = ['Иван','Максим','Светлана','Олег','Ольга','Елена','Артем'];
+
+  // eslint-disable-next-line id-length
+  for (let i = 0; i < 25; i++ ) {
+    photoArr.push({
+      id: i + 1,
+      url: `photos/${i + 1}.jpg`,
+      description: 'Описание фотографии',
+      likes: getRandomInt(15,200),
+      comments: [
+        {
+          id: i + 5,
+          avatar: `img/avatar-${getRandomInt(1,6)}.svg`,
+          message: `${commentsArr[getRandomInt(0,2)]} ${commentsArr[getRandomInt(3,5)]}`,
+          name: namesArr[getRandomInt(0,5)],
+        },
+      ],
+    });
+  }
+
+  return photoArr;
 };
 
-getRandomInt(1,25);
-
-const createPhotoItem = () => {
-  return {
-    id: '',
-    url: '',
-    description: '',
-    likes: '',
-    comments: [
-      {
-        id: '',
-        avatar: '',
-        message: '',
-        name: '',
-      },
-    ],
-  };
-};
-
-createPhotoItem();
+getPhotosArr();
