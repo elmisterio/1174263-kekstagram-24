8334d9a6a503eceb44d1bb2b4bdb77564f083765
StanislavSmolyanko <st.smolyanko@gmail.com>
2021-10-14 13:52:16 +0300

    add createMiniPhotos function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-unused-vars */
 
 import {getRandomInt, isStringFit} from './utils.js';
-import {createCommentsArr, createPhotosArr} from './data.js';
+import {createCommentsArr, createPhotosArr, createMiniPhotos} from './data.js';
